1471b686e6d0f48bd02b99a16642c8825d0ec331
Vito <topvetal@gmail.com>
2021-03-08 23:19:54 +0200

    last_commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import './styles.css';
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+import $ from 'jquery';
+import menu from '../menu'
+import list from './list.hbs'
+
+const
+  toggle = $('#theme-switch-toggle'),
+  body = $('body'),
+  ul = $('ul.js-menu'),
+  theme = { LIGHT: 'light-theme', DARK: 'dark-theme' };
+
+// Generate menu
+ul.append(list(menu))
+
+// Check theme in localStorage
+let checkLocalStorage = () => localStorage.getItem('theme') === theme.DARK ? theme.DARK : theme.LIGHT
+
+// Change theme style
+let toggleChange = () => checkLocalStorage() === theme.LIGHT ? theme.DARK : theme.LIGHT
+// Theme update
+function update() {
+  let tmp = checkLocalStorage()
+  body.removeClass().addClass(tmp)
+  localStorage.setItem('theme', tmp)
+  toggle.attr('checked', tmp === theme.DARK)
+}
+
+toggle.on('change', evt => {
+  localStorage.setItem('theme', toggleChange())
+  update()
+})
+
+update()
+
